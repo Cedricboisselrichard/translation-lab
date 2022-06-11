d4a0d42386b2559266604f5e5bc79a7808491b51
cboissel <cedricboisselrichard@gmail.com>
2022-06-11 23:39:58 +0200

    Init commit 2

---
+++ src/main/java/com/cboissel/translationLab/TranslationLabApplication.java
@@ -0,0 +1,13 @@
+package com.cboissel.translationLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslationLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cboissel/translationLab/TranslationLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cboissel.translationLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslationLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
