9222582521ae7f135d15e81817e59617ee01cdc9
cboissel <cedricboisselrichard@gmail.com>
2022-06-14 00:12:22 +0200

    Rename project and package

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# translationLab
+# translation-lab
 
--- src/main/java/com/cboissel/translation_lab/TranslationLabApplication.java
+++ src/main/java/com/cboissel/translation_lab/TranslationLabApplication.java
@@ -1,4 +1,4 @@
-package com.cboissel.translationLab;
+package com.cboissel.translation_lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
